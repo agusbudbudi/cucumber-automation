199a4edfbb692cd19d585ae6e1e7367ed1cddda2
Agus Budiman <90586227+v-agusbudiman@users.noreply.github.com>
2022-07-10 19:35:15 +0700

    Initial commit

---
+++ src/test/java/StepDef/TestSearchGoogle.java
@@ -0,0 +1,26 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TestSearchGoogle {
+    @Given("saya Buka browser")
+    public void sayaBukaBrowser() {
+    }
+
+    @And("Buka situs web Google")
+    public void bukaSitusWebGoogle() {
+    }
+
+    @And("Terletak di situs web google")
+    public void terletakDiSitusWebGoogle() {
+    }
+
+    @When("saya mencari {string}")
+    public void sayaMencari(String arg0) {
+    }
+
+    @Then("Menampilkan hasil yang terkait dengan {string}")
+    public void menampilkanHasilYangTerkaitDengan(String arg0) {
+    }
+}
